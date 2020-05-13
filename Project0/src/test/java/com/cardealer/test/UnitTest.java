1e7916a2d937587c54f7955a3805e70979f470b3
Casey Spencer <spencerc2009@my.fit.edu>
2020-05-13 10:21:58 -0400

    fixed logging

--- Project0/src/test/java/com/cardealer/test/UnitTest.java
+++ Project0/src/test/java/com/cardealer/test/UnitTest.java
@@ -7,9 +7,18 @@ import org.junit.jupiter.api.Test;
 class UnitTest {
 
 	@Test
-	void test() {
+	void testDecimals() {
 		assertEquals(1000.00/25, 40.00); //Essentially the math I run to determine the monthly bill using
 		//the formula of total cost divided by number of months left. Test shows typing doesn't throw anything off
+	}
+	void testPrecisionLossThroughHandoff() {
+		//showing the same functionality works correctly even after multiple variable handoffs as occurs during the program
+		double a=5.723451209898;
+		double b=a;
+		double c=b;
+		double d=c;
+		double e=d;
+		assertEquals(e, 5.723451209898);
 	}
 
 }
